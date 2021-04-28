a3a2aa40e672e504c3bd28c5ad4da73269e732f1
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-04-28 12:24:43 -0600

    Implement Full-text Search
    We synchronize ElasticSearch with DynamoDb by basically updating our lambda function to read Data from DynamoDBStream and replicate them to ES
    
    TOW files to pay attention to in this commit severless.yml and /elasticSearchSync/main.go
    
    The approach we took here was to use the REST API method to sync the data to ES. This is the approach i leaned towards more
    https://www.elastic.co/guide/en/elasticsearch/reference/6.8/docs-index_.html
    
    You may considering using third party libraries, but there is no stable package in golang yet. They advise you to use the libraries for development and not Production. Even elasticSearch go package itself is still under works
    https://github.com/Ouest-France/lambda-flowlogs-ingester/blob/master/main.go
    
    FYI
    You must sign the request in order to use our to ES Cluster. Right now everyone can use it. But you can limit the user like this link below but now, the request to the cluster will be signed by that particular aws user
    https://stackoverflow.com/questions/41725728/have-issues-access-to-es-with-aws-request-signing
    Notice how he assign a user in his Principal.AWS
    
    The step i followed in this commit is here for signing REST API request to aws services
    https://docs.aws.amazon.com/elasticsearch-service/latest/developerguide/es-request-signing.html#es-request-signing-go
    https://www.bogotobogo.com/DevOps/AWS/aws-Amazon-Loading-DynamoDB-Stream-to-ElasticSearch-with-Lambda.php (hit on how to delete a request with REST API ES)
    
    Another thing to note is if you are to handle record updates like MODIFY, REMOVE in addition to INSERT that we just implemented here, consider using switch statement like below
    https://github.com/serverless/examples/blob/master/aws-golang-dynamo-stream-to-elasticsearch/cmd/aws-golang-dynamo-stream-to-elasticsearch/main.go
    
    Some reason you might get 403 forbidden error request to your cluster
    https://stackoverflow.com/questions/65033582/aws-elasticsearch-returning-403-forbidden-error-using-es-client
    
    Additional Resources
    https://github.com/serverless/examples/blob/master/aws-golang-dynamo-stream-to-elasticsearch/cmd/aws-golang-dynamo-stream-to-elasticsearch/main.go
    https://gist.github.com/morcmarc/35194b39184977c2c44eb9fe063889a3https://gitlab.com/gitlab-org/gitlab-elasticsearch-indexer/blob/a48ad27223c5402a703e165bbca286203f6668e8/elastic/client.go

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/aws/aws-lambda-go v1.23.0
-	github.com/aws/aws-sdk-go v1.38.25 // indirect
+	github.com/aws/aws-sdk-go v1.38.26 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/satori/go.uuid v1.2.0 // indirect
 )
--- src/lambda/dynamoDb/elasticSearchSync/main.go
+++ src/lambda/dynamoDb/elasticSearchSync/main.go
@@ -4,22 +4,110 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	v4 "github.com/aws/aws-sdk-go/aws/signer/v4"
 )
 
 type DynamoDBStreamEvent events.DynamoDBEvent
 
-func elasticSearchSyncHandler(e DynamoDBStreamEvent) {
+type Body struct {
+	ImageId   string `json:"imageId"`
+	GroupId   string `json:"groupId"`
+	Title     string `json:"title"`
+	Timestamp string `json:"timestamp"`
+	ImageUrl  string `json:"imageUrl"`
+}
+
+var (
+	signer  *v4.Signer
+	url     string
+	esHost  = fmt.Sprintf("https://%s", os.Getenv("ES_ENDPOINT"))
+	index   = "images-index"
+	t       = "images" //the type, as required by ES as the path to your indicies
+	service = "es"
+	region  = "ca-central-1"
+)
+
+func init() {
+
+	// Get credentials from environment variables and create the AWS Signature Version 4 signer
+	credentials := credentials.NewStaticCredentials("AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "") //TODO: Read the secret from secret manager
+	signer = v4.NewSigner(credentials)
+
+	url = esHost + "/" + index + "/" + t + "/"
+}
+
+func elasticSearchSyncHandler(e DynamoDBStreamEvent) error {
 
 	r, _ := json.MarshalIndent(e, "", " ")
 	log.Printf("Processing events batch from DynamoDB: %s", r)
 
 	for _, record := range e.Records {
 		fmt.Printf("Processing request data for event ID %s, type %s.\n", record.EventID, record.EventName)
+
+		/*
+			If the eventName is not 'INSERT', we will skip this record.
+
+			However, in a complete solution, we will remove record from ES if they are removed(REMOVE) from dynamodb
+			or update ES record if they are updated(MODIFY) in dynamoDb. Eg like in the link
+			https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_streams_Record.html
+		*/
+		if record.EventName != "INSERT" {
+			continue
+		}
+
+		//Get the item that was added to dynaoDb
+		newItem := record.Change.NewImage
+
+		id := newItem["imageId"].String()
+
+		/*
+			We create the document that we want to store in ElasticSearch.
+			We basically copied all fields from our dynamoDb item to our struct
+		*/
+		b := Body{
+			ImageId:   newItem["imageId"].String(),
+			Timestamp: newItem["timestamp"].String(),
+			GroupId:   newItem["groupId"].String(),
+			ImageUrl:  newItem["imageUrl"].String(),
+			Title:     newItem["title"].String(),
+		}
+
+		// JSON document to be included as the request body
+		out, _ := json.Marshal(b)
+		body := strings.NewReader(string(out))
+
+		// An HTTP client for sending the request
+		client := &http.Client{}
+
+		// Form the HTTP request. We are making a PUT requet as specified in ES doc
+		//https://www.elastic.co/guide/en/elasticsearch/reference/6.8/docs-index_.html
+		req, err := http.NewRequest(http.MethodPut, url+id, body)
+		if err != nil {
+			fmt.Print(err)
+		}
+
+		// You can probably infer Content-Type programmatically, but here, we just say that it's JSON
+		req.Header.Add("Content-Type", "application/json")
+
+		// Sign the request, send it, and print the response
+		signer.Sign(req, body, service, region, time.Now())
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Print(err)
+		}
+		fmt.Print(resp.Status + "\n")
+
 	}
 
+	return nil
 }
 
 func main() {
